c54b00d2ce5a5fc1e54c7a3c753a52c4d8c8fbed
Rafael Blanco <rafblanco93@gmail.com>
2019-05-07 12:44:17 -0400

    Project structure and Redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();
+import './index.css';
+import * as registerServiceWorker from './registerServiceWorker';
+
+import store from './store';
+import App from './components/App';
+import Firebase, { FirebaseContext } from './components/Firebase';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <FirebaseContext.Provider value={new Firebase()}>
+      <App />
+    </FirebaseContext.Provider>
+  </Provider>,
+  document.getElementById('root'),
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+registerServiceWorker.unregister();
